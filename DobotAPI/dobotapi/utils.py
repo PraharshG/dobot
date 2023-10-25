24e92f32141dfa4c772d48e188582771f2b7d134
Praharsh <108566216+PraharshG@users.noreply.github.com>
2023-10-25 15:05:56 +0530

    Update utils.py
    removed the return type of the function get_com_ports

--- DobotAPI/dobotapi/utils.py
+++ DobotAPI/dobotapi/utils.py
@@ -3,7 +3,7 @@ from serial.tools import list_ports
 from .exceptions import NoComportsAvaliable
 
 
-def get_coms_port() -> list[str]:
+def get_coms_port():
     comports = list_ports.comports()
 
     if comports:
